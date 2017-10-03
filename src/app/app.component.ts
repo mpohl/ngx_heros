252cb9bcec7d60163655bc2da69ecb9118d0d8f3
Marko Pohl <marko.pohl@googlemail.com>
2017-10-04 00:16:10 +0200

    add description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {Title} from '@angular/platform-browser';
+import {Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-my-app',
@@ -11,15 +11,28 @@ export class AppComponent {
   title = 'Tour of Heroes';
   param = {value: 'world'};
 
-  constructor(translate: TranslateService, titleService: Title) {
+  constructor(translate: TranslateService, titleService: Title, metaService: Meta) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
 
+    /**
+     * set browser title
+     */
     translate.get('app.title').subscribe((res: string) => {
         titleService.setTitle(res);
       });
+    /**
+     * set browser descr
+     */
+    translate.get('app.description').subscribe((res: string) => {
+      metaService.addTag({
+        name: 'description',
+        content: res
+      });
+    });
+
   }
 }
