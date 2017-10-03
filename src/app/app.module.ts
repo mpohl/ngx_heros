519dfe0221313aada9c027cc0c8ef95eb1e3399d
Marko Pohl <marko.pohl@googlemail.com>
2017-10-03 21:17:52 +0200

    add ngx translate and extract, get lang files by http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
-
+import {AppRoutingModule} from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './_services/in-memory-data.service';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './_services/in-memory-data.service';
 
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './hero/heroes.component';
-import { HeroDetailComponent }  from './hero/detail/hero-detail.component';
-import { HeroService }          from './_services/hero.service';
-import { HeroSearchComponent }  from './hero/search/hero-search.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroesComponent} from './hero/heroes.component';
+import {HeroDetailComponent} from './hero/detail/hero-detail.component';
+import {HeroService} from './_services/hero.service';
+import {HeroSearchComponent} from './hero/search/hero-search.component';
+
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   imports: [
@@ -22,7 +30,15 @@ import { HeroSearchComponent }  from './hero/search/hero-search.component';
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
   ],
   declarations: [
     AppComponent,
@@ -31,7 +47,8 @@ import { HeroSearchComponent }  from './hero/search/hero-search.component';
     HeroesComponent,
     HeroSearchComponent
   ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
