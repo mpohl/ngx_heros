f2ac0ce2261549d4dcf8250857e6aa073ac65203
Marko Pohl <marko.pohl@googlemail.com>
2017-10-04 20:51:50 +0200

    move service

--- src/app/hero/search/hero-search.component.ts
+++ src/app/hero/search/hero-search.component.ts
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 
-import { HeroSearchService } from './hero-search.service';
+import { HeroSearchService } from '../../_services/hero-search.service';
 import {Hero} from '../hero';
 
 @Component({
