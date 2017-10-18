5ea3b2a551fc3cedbe252c67e97434a800b1a2bb
Marko Pohl <marko.pohl@googlemail.com>
2017-10-18 10:28:39 +0200

    add lang attribute to <html>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import {Inject} from '@angular/core';
 import {Component} from '@angular/core';
 import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
 import {Meta, Title} from '@angular/platform-browser';
+import {DOCUMENT} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-my-app',
@@ -12,7 +14,8 @@ export class AppComponent {
 
   constructor(private translate: TranslateService,
               private titleService: Title,
-              private metaService: Meta) {
+              private metaService: Meta,
+              @Inject(DOCUMENT) private _document: any) {
     /**
      * Set default lang
      * this language will be used as a fallback when a translation isn't found in the current language
@@ -22,12 +25,13 @@ export class AppComponent {
     /**
      * the lang to use, if the lang isn't available, it will use the current loader to get them
      */
-    translate.use('en').subscribe((res: string) => {
-      /*
-      this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-        console.log('onLangChange', event);
-      });
-      */
+    translate.use('en');
+
+    /**
+     * set html lang attribute
+     */
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this._document.documentElement.lang = event.lang;
     });
 
     /**
