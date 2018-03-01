33fcb679f18575c9ad495d86b10693e6fe687c3f
Marko Pohl <marko.pohl@googlemail.com>
2018-03-01 13:04:15 +0100

    add submodule and fix router translation prefix

---
+++ src/app/submodule/submodule/submodule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './submodule.component.html',
+  styleUrls: ['./submodule.component.scss']
+})
+export class SubmoduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
