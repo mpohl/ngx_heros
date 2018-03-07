3ec385f297097c685c8898c1611770a8fd6b3ae1
Marko Pohl <marko.pohl@googlemail.com>
2018-03-07 22:13:19 +0100

    add guarded url as query param

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 ﻿import {Injectable} from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
+import {Router, CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot} from '@angular/router';
 import {LocalizeRouterService} from 'localize-router';
 
 @Injectable()
@@ -8,7 +8,10 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private localize: LocalizeRouterService) {
   }
 
-  canActivate() {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) {
     if (localStorage.getItem('currentUser')) {
       // logged in so return true
       return true;
@@ -16,7 +19,7 @@ export class AuthGuard implements CanActivate {
 
     // not logged in so redirect to login page
     const translatedPath: any = this.localize.translateRoute('/login');
-    this.router.navigate([translatedPath]);
+    this.router.navigate([translatedPath], { queryParams: { url: state.url } });
     return false;
   }
 }
